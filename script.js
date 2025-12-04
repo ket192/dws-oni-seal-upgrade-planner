ee7743b95bda6aab0fbb0f2129e50bfcf0bd7362
ket192 <danihahayward@gmail.com>
2025-12-05 12:53:21 +1300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ let oniLevels = [
   { level: 13, quartz: 486900, upgradeTime: "05:38:28" },
   { level: 14, quartz: 584300, upgradeTime: "06:00:07" },
   { level: 15, quartz: 701200, upgradeTime: "06:23:56" },
-  { level: 16, quartz: 771300, upgradeTime: "07:51:05" }
+  { level: 16, quartz: 771300, upgradeTime: "07:51:05" },
+  { level: 17, quartz: 848400, upgradeTime: "08:19:54" },
+  { level: 18, quartz: 933300, upgradeTime: "08:51:37" }
 ];
 
 const ONI_STORAGE_KEY = "oniSealHallLevels_v1";
@@ -330,3 +332,4 @@ document.addEventListener("DOMContentLoaded", () => {
     calendarBtn.addEventListener("click", downloadCalendarEvent);
   }
 });
+
